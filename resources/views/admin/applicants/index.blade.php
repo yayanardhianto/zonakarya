1773cc135c5a823615b3dd5703963c05a92a7af0
yayanardhianto <yayanardhianto@users.noreply.github.com>
2025-11-13 14:14:37 +0700

    update request

--- resources/views/admin/applicants/index.blade.php
+++ resources/views/admin/applicants/index.blade.php
@@ -211,7 +211,7 @@
                                         <td class="text-small py-2">
                                                 <span class="badge badge-info">{{ $application->jobVacancy->position }}</span>
                                                 @if($application->jobVacancy->location)
-                                                    <br><small class="text-muted"><i class="fas fa-map-marker-alt text-small"></i> {{ $application->jobVacancy->location }}</small>
+                                                    <br><span class="text-muted text-small mt-1"><i class="fas fa-map-marker-alt text-small me-1"></i> {{ $application->jobVacancy->location }}</span>
                                                 @endif
                                         </td>
                                         <td>
